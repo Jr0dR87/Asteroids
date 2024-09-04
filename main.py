aacc554ffe5c7c927e115ebfc9b7c39e2aace7ab
Jarrod <me@jarrodrizor.com>
2024-09-03 20:49:13 -0400

    Update main.py
    Changes to set framerate.

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import pygame
 import constants
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 
 
     while True:
-        pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill("black")
+        pygame.display.flip()
+
+        #set framerate
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
