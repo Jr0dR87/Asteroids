1d2f41d602d95e2f4a416e073e7f9d4ee94b2b46
Jarrod <me@jarrodrizor.com>
2024-09-05 19:57:27 -0400

    Update main.py
    Logic to make the player class move and work.

--- main.py
+++ main.py
@@ -2,23 +2,38 @@ import pygame
 from constants import *
 from player import Player
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0 
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        #set framerate
+
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
